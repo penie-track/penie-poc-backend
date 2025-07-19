c95aee6006348adf4b21af04f3aab7c1d9a7b18c
gnana <pradeepgundluru077@gmail.com>
2025-07-19 18:14:16 +0530

    project setup

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import connectDB from "./config/db";
+import routes from "./routes";
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api", routes);
+
+// Start Server
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+});
