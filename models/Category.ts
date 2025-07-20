30661e3af347192060ccacfdd4d8466a0e84cf3e
Saleh Abdullahi <salehabdullahi018@gmail.com>
2025-07-20 10:12:15 +0100

    created user & category models

---
+++ models/Category.ts
@@ -0,0 +1,10 @@
+import mongoose, { Schema } from "mongoose";
+
+const categorySchema = new Schema({
+  userId: { type: Schema.Types.ObjectId, ref: "User" },
+  name: { type: String },
+  type: { type: String, enum: ["income", "expense"] },
+  color: { type: String },
+  icon: { type: String },
+  createdAt: { type: Date, default: Date.now },
+});
